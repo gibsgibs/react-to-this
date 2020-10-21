35829a36e29bf8579d130fe5494fdc8140368526
gibsgibs-desktop <g.brehm@hotmail.com>
2020-10-21 16:39:55 -0700

    Updated .gitignore to ignore .vscode file.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router';
 import 'jquery/dist/jquery.slim';
 import 'bootstrap/dist/js/bootstrap.bundle';
-import './scss/main.scss'
+import './scss/main.scss';
 
 import { Navbar } from './components/Navbar/Navbar';
 import { Home } from './components/home';
