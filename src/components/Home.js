d8afa457e5fa656811df5cd61116656d7e9fcc4b
gibsgibs-desktop <g.brehm@hotmail.com>
2020-10-24 13:54:57 -0700

    Updated the home component to use the server.

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,12 +21,20 @@ export class Home extends Component {
     }
     render() {
         return(
-            <div className="text-center">
+            <div className="text-center container">
                 <h1>This is the home component</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <label htmlFor="name">Enter your name: </label>
-                    <input id="name" type="text" value={this.state.name} onChange={this.handleChange} />
-                    <button type="submit">Submit</button>
+                    <div className="form-group">
+                        <div className="input-group">
+                            <div className="input-group-prepend">
+                                <div className="input-group-text bg-primary">
+                                    <label className="m-0 px-1">Enter your name</label>
+                                </div>
+                            </div>
+                            <input className="form-control" id="name" type="text" value={this.state.name} onChange={this.handleChange} />
+                        </div>
+                    </div>
+                    <button className="btn btn-primary" type="submit">Submit</button>
                 </form>
                 <p>{this.state.greeting}</p>
             </div>
