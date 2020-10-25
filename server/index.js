1e22ff8fec70a3620618e0a993195069334e0dc8
gibsgibs-desktop <g.brehm@hotmail.com>
2020-10-24 21:27:55 -0700

    Added something to the index.js server for production, and changed the styling to keep the menu on the top.

--- server/index.js
+++ server/index.js
@@ -12,6 +12,14 @@ app.get('/api/greeting', (req, res) => {
     res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
+
 app.listen(3001, () =>
     console.log('Express server is running on localhost:3001')
 );
